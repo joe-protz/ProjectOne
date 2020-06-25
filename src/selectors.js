e6fa59857e06bc58ab57c011feb439f02138b522
Joe <joe10994@gmail.com>
2020-06-25 10:55:09 -0400

    Add logic to toggle boxes; Improve styles
    - Added event listeners for buttons to toggle evens & odds
    - Used css Calc to create correct container height
    - Cleaned up code
    - Added arrow-parens:off to eslintrc

--- src/selectors.js
+++ src/selectors.js
@@ -3,11 +3,16 @@ const evenButton = document.getElementById('even')
 const oddButton = document.getElementById('odd')
 
 const boxes = document.getElementsByClassName('box')
+const oddBoxes = document.getElementsByClassName('odd')
+const evenBoxes = document.getElementsByClassName('even')
+
 const boxArr = [...boxes]
+const oddArr = [...oddBoxes]
+const evenArr = [...evenBoxes]
+const buttonArr = [oddButton, evenButton, allButton]
 
 const toggleActive = e => {
   e.target.classList.add('selected')
-  const buttonArr = [oddButton, evenButton, allButton]
   buttonArr.splice(buttonArr.indexOf(e.target), 1)
   buttonArr.forEach(button => button.classList.remove('selected'))
 }
@@ -15,20 +20,28 @@ const toggleActive = e => {
 const allListener = () => {
   allButton.addEventListener('click', e => {
     toggleActive(e)
-  })
-
-  boxArr.forEach(box => {
-    box.classList.remove('hidden')
+    boxArr.forEach(box => {
+      box.classList.remove('hidden')
+    })
   })
 }
 const oddListener = () => {
   oddButton.addEventListener('click', e => {
     toggleActive(e)
+    // uses a for loop to save on complexity
+    for (let i = 0; i < oddArr.length; i++) {
+      evenArr[i].classList.add('hidden')
+      oddArr[i].classList.remove('hidden')
+    }
   })
 }
 const evenListener = () => {
   evenButton.addEventListener('click', e => {
     toggleActive(e)
+    for (let i = 0; i < evenArr.length; i++) {
+      evenArr[i].classList.remove('hidden')
+      oddArr[i].classList.add('hidden')
+    }
   })
 }
 
